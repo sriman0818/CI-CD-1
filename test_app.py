e34b7dd3cfb758e7e4124c38af70afa19091853d
SRIMAN <sriman0818@gmail.com>
2026-05-08 13:21:39 +0530

    Add pytest integration and new tests for app

--- test_app.py
+++ test_app.py
@@ -1,4 +1,16 @@
-from app import add
+import pytest
+from app import app
 
-def test_add():
-    assert add(2, 3) == 5
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    r = client.get('/')
+    assert r.status_code == 200
+
+def test_health(client):
+    r = client.get('/health')
+    assert r.status_code == 200
